295309f5778a1e1ce6033c2d138ddf312681b144
Mark Kennedy <kennedymark@hotmail.com>
2022-05-05 21:02:25 -0400

    Added seed file for movies

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const express = require('express')
+const app = express()
 const cors = require('cors')
-// const logger = require('morgan')
 
+// const logger = require('morgan')
 // const AuthRouter = require('./routes/AuthRouter')
 // const PilgrimRouter = require('./routes/PilgrimRouter')
-
-const app = express()
+// app.use(logger('dev'))
 
 const AppRouter = require('./routes/AppRouter')
 
 const PORT = process.env.PORT || 3001
 
 app.use(cors())
-// app.use(logger('dev'))
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 // app.use('/auth', AuthRouter)
 // app.use('/pilgrim', PilgrimRouter)
---
+++ seeders/20220506005129-Movie.js
@@ -0,0 +1,37 @@
+'use strict'
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    let movies = [
+      {
+        name: 'Predator',
+        description:
+          'Dutch (Arnold Schwarzenegger), a soldier of fortune, is hired by the U.S. government to secretly rescue a group of politicians trapped in Guatemala.',
+        image:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRikSEIgYlliuIjkbfy1dm_7eQ0Ee1R2YZp_mOwY8VFLFCa5Acy97-NF3Mq4RvYqfY38_s&usqp=CAU',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ]
+    await queryInterface.bulkInsert('movies', movies)
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('movies')
+  }
+}
