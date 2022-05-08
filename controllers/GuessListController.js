95d7f0e9c4adbf012bad83426838f27bed1530c0
Mark Kennedy <kennedymark@hotmail.com>
2022-05-08 09:03:43 -0400

    created route for GetGuessListByMovieId.

--- controllers/GuessListController.js
+++ controllers/GuessListController.js
@@ -2,16 +2,29 @@ const { GuessList } = require('../models')
 
 const CreateGuessList = async (req, res) => {
   try {
-    let guesslistId = parseInt(req.params.guesslist_id)
-    const characterBody = {
-      guesslistId,
+    let movieId = parseInt(req.params.movie_id)
+    const guessListBody = {
+      movieId,
       ...req.body
     }
-    const character = Character.create(characterBody)
-    res.send(character)
-  } catch (error) {}
+    const guessList = GuessList.create(guessListBody)
+    res.send(guessList)
+  } catch (error) {
+    throw error
+  }
+}
+
+const GetGuessListByMovieId = async (req, res) => {
+  try {
+    const movieId = parseInt(req.params.movie_id)
+    let guessLists = await GuessList.findAll({ where: { movieId: movieId } })
+    res.send(guessLists)
+  } catch (error) {
+    throw error
+  }
 }
 
 module.exports = {
-  CreateGuessList
+  CreateGuessList,
+  GetGuessListByMovieId
 }
--- routes/GuessListRouter.js
+++ routes/GuessListRouter.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const controller = require('../controllers/GuessListController')
 
-router.post('/', controller.CreateGuessList)
+router.post('/:movie_id', controller.CreateGuessList)
+router.get('/:movie_id', controller.GetGuessListByMovieId)
 
 module.exports = router
