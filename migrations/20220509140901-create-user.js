8433abac35c63b3e467b268bd2be3879e9c60e06
Mark Kennedy <kennedymark@hotmail.com>
2022-05-09 10:32:17 -0400

    creating auth, model for user, auth router, controller, middleware

---
+++ migrations/20220509140901-create-user.js
@@ -0,0 +1,42 @@
+'use strict'
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      passwordDigest: {
+        type: Sequelize.STRING
+      },
+      movieId: {
+        type: Sequelize.INTEGER
+      },
+      guessListId: {
+        type: Sequelize.INTEGER
+      },
+      admin: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users')
+  }
+}
--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -1,9 +1,11 @@
 const Router = require('express').Router()
+const AuthRouter = require('./AuthRouter')
 const MovieRouter = require('./MovieRouter')
 const CastMemberRouter = require('./CastMemberRouter')
 const CharacterRouter = require('./CharacterRouter')
 const GuessListRouter = require('./GuessListRouter')
 
+Router.use('/auth', AuthRouter)
 Router.use('/movie', MovieRouter)
 Router.use('/castmember', CastMemberRouter)
 Router.use('/character', CharacterRouter)
